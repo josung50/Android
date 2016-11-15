a11d2295242f0ddbd643980c03ce394acd001d01
josung50 <josung50@naver.com>
2016-11-15 14:19:15 +0900

    MyLogger ver 1.0

--- app/src/main/java/opensource/mobileprogram/DBHelper2.java
+++ app/src/main/java/opensource/mobileprogram/DBHelper2.java
@@ -6,11 +6,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
- * Created by JSR on 2016-10-31.
+ * Created by JSR on 2016-11-10.
  */
-public class DBHelper extends SQLiteOpenHelper{
+public class DBHelper2 extends SQLiteOpenHelper{
     // DBHelper 생성자로 관리할 DB 이름과 버전 정보를 받음
-    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public DBHelper2(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
@@ -18,7 +18,7 @@ public class DBHelper extends SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase db) {
         // 새로운 테이블 생성
-        db.execSQL("CREATE TABLE list (_id INTEGER PRIMARY KEY AUTOINCREMENT, lat REAL , lng REAL , address TEXT);");
+        db.execSQL("CREATE TABLE list2 (_id INTEGER PRIMARY KEY AUTOINCREMENT, lat REAL , lng REAL , address TEXT , doing TEXT , hour TEXT , minute TEXT , date TEXT , detail TEXT);");
     }
 
     // DB 업그레이드를 위해 버전이 변경될 때 호출되는 함수
@@ -28,16 +28,16 @@ public class DBHelper extends SQLiteOpenHelper{
 
     public void DropTalbe(){
         SQLiteDatabase db = getWritableDatabase();
-        String sql = "DROP TABLE list";
+        String sql = "DROP TABLE list2";
         db.execSQL(sql);
         onCreate(db);
     }
 
-    public void insert(double lat, double lng, String address) {
+    public void insert(double lat, double lng, String address , String doing , String hour , String minute , String date , String detail) {
         // 읽고 쓰기가 가능하게 DB 열기
         SQLiteDatabase db = getWritableDatabase();
         // DB에 입력한 값으로 행 추가
-        db.execSQL("INSERT INTO list VALUES(null, " + lat + ", " + lng + ", '" + address + "');");
+        db.execSQL("INSERT INTO list2 VALUES(null, " + lat + ", " + lng + ", '" + address +  "','" + doing + "','" + hour + "','" + minute + "','" + date + "','" + detail + "');");
         db.close();
     }
 
@@ -47,7 +47,7 @@ public class DBHelper extends SQLiteOpenHelper{
         String result = "";
 
         // DB에 있는 데이터를 쉽게 처리하기 위해 Cursor를 사용하여 테이블에 있는 모든 데이터 출력
-        Cursor cursor = db.rawQuery("SELECT * FROM list", null);
+        Cursor cursor = db.rawQuery("SELECT * FROM list2", null);
         while (cursor.moveToNext()) {
             result += cursor.getString(0)
                     + " : "
@@ -56,8 +56,17 @@ public class DBHelper extends SQLiteOpenHelper{
                     + cursor.getInt(2)
                     + ", "
                     + cursor.getString(3)
+                    + ", "
+                    + cursor.getString(4)
+                    + ", "
+                    + cursor.getString(5)
+                    + ", "
+                    + cursor.getString(6)
+                    + ", "
+                    + cursor.getString(7)
                     + "\n";
         }
         return result;
     }
 }
+
--- app/src/main/java/opensource/mobileprogram/Mymap.java
+++ app/src/main/java/opensource/mobileprogram/Mymap.java
@@ -3,10 +3,12 @@ package opensource.mobileprogram;
 import android.*;
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.app.Service;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -27,6 +29,8 @@ import android.util.Log;
 import android.view.View;
 import android.webkit.PermissionRequest;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -46,7 +50,8 @@ import java.util.Locale;
 
 public class Mymap extends FragmentActivity implements OnMapReadyCallback{
     // DB 관련 변수들
-    final DBHelper dbHelper = new DBHelper(this, "list.db", null , 1);
+    final DBHelper dbHelper = new DBHelper(this, "list.db", null , 1); // 행적 추적을 위한 DB
+    final DBHelper2 dbHelper2 = new DBHelper2(this, "list2.db", null , 1); // 일지를 위한 DB
 
     public static int count = 0;
     private final int ID_REQUEST_PERMISSION = 0x00;
@@ -58,6 +63,8 @@ public class Mymap extends FragmentActivity implements OnMapReadyCallback{
 
     static GoogleMap mMap;
     static MapFragment mapFragment;
+    static public LatLng LATLNG;
+    static RelativeLayout temp; // 일지 정보를 보여주기 위한 레이아웃 , 혹은 통계
 
     LocationManager manager;
 
@@ -65,6 +72,10 @@ public class Mymap extends FragmentActivity implements OnMapReadyCallback{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mymap);
+
+        // 마커의 윈도우 창 투명하게
+        temp = (RelativeLayout)findViewById(R.id.temp);
+        temp.setVisibility(View.INVISIBLE);
 
         String[] REQUEST_PERMISSIONS = {
                 Manifest.permission.ACCESS_FINE_LOCATION ,
@@ -80,8 +91,7 @@ public class Mymap extends FragmentActivity implements OnMapReadyCallback{
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-
-
+        Toast.makeText(getApplicationContext(), "일지를 작성하고자 하면 지도를 꾹 눌러주세요." , Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -92,6 +102,58 @@ public class Mymap extends FragmentActivity implements OnMapReadyCallback{
     @Override
     public void onMapReady(GoogleMap map) {
         mMap = map;
+
+        // 마커의 윈도우 클릭하면 확대
+        map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                // 저장했던 시간 , 장소 , 얼마나 , 했던 일 , 세부사항
+                temp.setVisibility(View.VISIBLE);
+                TextView view1 = (TextView)findViewById(R.id.view1);
+                TextView view2 = (TextView)findViewById(R.id.view2);
+                view1.setText(marker.getTitle());
+                view1.setTextSize(25);
+                view2.setText(marker.getSnippet());
+                view2.setTextSize(25);
+            }
+        });
+
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latlng) {
+                LATLNG = latlng; // static latlng에 파라미터를 대입... 액티비티 전환용
+                Log.i("LATLNG", "LATLNG" + LATLNG);
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions()
+                        .position(latlng)
+                        .title("Your click location"));
+
+                // 예 아니오 팝업 창 띄우기
+                AlertDialog.Builder alert_confirm = new AlertDialog.Builder(Mymap.this);
+                alert_confirm.setMessage("이 위치에서 일지를 작성하시겠습니까?").setCancelable(false).setPositiveButton("작성",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // 'YES'
+                                Intent intent = new Intent(Mymap.this, Write.class);
+                                startActivity(intent);
+
+
+                            }
+                        }).setNegativeButton("취소",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // 'No'
+                                return;
+                            }
+                        });
+                AlertDialog alert = alert_confirm.create();
+                alert.show();
+
+            }
+        });
+
         map.addMarker(new MarkerOptions()
                 .position(new LatLng(0, 0))
                 .title("Marker"));
@@ -100,7 +162,13 @@ public class Mymap extends FragmentActivity implements OnMapReadyCallback{
     // 행적 초기화
     public void DropButton(View v) {
         dbHelper.DropTalbe();
-        Log.i("드랍" , "성공");
+        Log.i("드랍", "성공");
+    }
+
+    // 일지 초기화
+    public void DropButton2(View v) {
+        Log.i("드랍2", "성공");
+        dbHelper2.DropTalbe();
     }
 
     // 현재위치 버튼 클릭
@@ -137,6 +205,7 @@ public class Mymap extends FragmentActivity implements OnMapReadyCallback{
         pl.color(Color.RED);
         pl.width(5);
         ArrayList points = new ArrayList<LatLng>();
+
         while(c.moveToNext())
         {
             count = c.getInt(c.getColumnIndex("_id"));
@@ -261,5 +330,105 @@ public class Mymap extends FragmentActivity implements OnMapReadyCallback{
             Toast.makeText(mContext, "Can't find address." , Toast.LENGTH_LONG).show();
         }
         return LocationName;
+    }
+
+    // 일지 보기
+    public void button(View v) { // 일지 보는 버튼
+        int Tcount;
+        String TAddress = null;
+        double Tlat = 0.0;
+        double Tlng = 0.0;
+        String Thour = null;
+        String Tminute = null;
+        String Tdoing = null;
+        String Tdetail = null;
+        String Tdate = null;
+        Cursor Tc = dbHelper2.getReadableDatabase().query("list2",null,null,null, null, null, null);
+
+        mMap.clear();
+        while(Tc.moveToNext()) {
+            Tcount = Tc.getInt(Tc.getColumnIndex("_id"));
+            TAddress = Tc.getString(Tc.getColumnIndex("address"));
+            Tlat = Tc.getDouble(Tc.getColumnIndex("lat"));
+            Tlng = Tc.getDouble(Tc.getColumnIndex("lng"));
+            Log.i("디비얻어오기", "성공" + Tlat + " " + Tlng);
+            Thour = Tc.getString(Tc.getColumnIndex("hour"));
+            Tminute = Tc.getString(Tc.getColumnIndex("minute"));
+            Tdoing = Tc.getString(Tc.getColumnIndex("doing"));
+            Tdetail = Tc.getString(Tc.getColumnIndex("detail"));
+            Tdate = Tc.getString(Tc.getColumnIndex("date"));
+            Log.i("정보" , "내용 :" + Tcount + " " + TAddress);
+            Marker marker = mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(Tlat, Tlng))
+                    .title(Tdate+": "+Tdoing)
+                    .snippet("장소 :"+TAddress+"\n"
+                                +"기간 :"+Thour+" "+Tminute+"\n"
+                                +"세부사항 :"+Tdetail));
+            //.snippet(TAddress + "\n" + Thour+" "+Tminute+"동안\n"+Tdetail));
+            marker.showInfoWindow();
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(Tlat , Tlng), 10.0f)); // 마지막 저장 값으로 이동
+    }
+
+    // 윈도우 창 닫기 버튼
+    public void close(View v) {
+        temp.setVisibility(View.INVISIBLE);
+    }
+
+    // 통계
+    public void rank(View v) {
+        TextView view1 = (TextView)findViewById(R.id.view1);
+        TextView view2 = (TextView)findViewById(R.id.view2);
+        int []list= new int[8] ;
+        String []rank = new String[8];
+        StringBuffer Binglist = new StringBuffer();
+        // list 초기화
+        for(int i=0; i<list.length; i++)
+            list[i] = 0;
+
+        String doing = null;
+        Cursor Tc = dbHelper2.getReadableDatabase().query("list2",null,null,null, null, null, null);
+        if(Tc == null)
+            Toast.makeText(getApplicationContext(), "Data가 없습니다." , Toast.LENGTH_LONG).show();
+
+        while(Tc.moveToNext()) {
+            doing = Tc.getString(Tc.getColumnIndex("doing"));
+            list[searchDoing(doing)]++;
+        }
+        for(int i=0; i<list.length; i++)
+        {
+            if(list[i] == 0)
+                continue;
+            switch(i){
+                case 0: rank[i] = "식사 :" + list[i] + "번\n"; break;
+                case 1: rank[i] = "과제 :" + list[i] + "번\n"; break;
+                case 2: rank[i] = "tv시청 :" + list[i] + "번\n"; break;
+                case 3: rank[i] = "게임 :" + list[i] + "번\n"; break;
+                case 4: rank[i] = "지인만나기 :" + list[i] + "번\n"; break;
+                case 5: rank[i] = "산책 :" + list[i] + "번\n"; break;
+                case 6: rank[i] = "독서 :" + list[i] + "번\n"; break;
+                case 7: rank[i] = "기타 :" + list[i] + "번\n"; break;
+            }
+            Binglist.append(rank[i]);
+        }
+        temp.setVisibility(View.VISIBLE);
+        view1.setText("한 일에 대한 통계");
+        view2.setText(Binglist);
+        view1.setTextSize(25);
+        view2.setTextSize(25);
+    }
+    // doing에 대한 매칭 결과 반환
+    public int searchDoing(String doing) {
+        switch(doing){
+            case "식사":  return 0;
+            case "과제":  return 1;
+            case "tv시청":    return 2;
+            case "게임":  return 3;
+            case "지인만나기":  return 4;
+            case "산책":  return 5;
+            case "독서":  return 6;
+            case "기타":  return 7;
+        }
+        return -1;
     }
 }
